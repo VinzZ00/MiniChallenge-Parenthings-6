86511cec1cbe462f7468ea5beaeffed02c598881
Elvin <elvin.ri39@gmail.com>
2023-04-30 00:45:13 +0700

    consultationMainPage Pickerand SIgnInPopUP

--- MiniChallenge-Parenthings-6/Features/Parenthings/Presenters/Views/ConsultationMainPageView.swift
+++ MiniChallenge-Parenthings-6/Features/Parenthings/Presenters/Views/ConsultationMainPageView.swift
@@ -14,7 +14,7 @@ struct ConsultationMainPageView: View {
     
     @ObservedObject var viewModel : parenthingsViewModel = parenthingsViewModel()
     
-    var x : [Expert] = [Expert(name: "Peter Parker", role: "Dokter Kandungan",education: "Dokter", educationDesc: "EducationDescription", longExp: 5, expDesc: "Experience Description", price: 20000, starCount: 4.5, imageBase64: (UIImage(named: "UniversalPlaceHolder")?.toBase64())!, isAvailable: false), Expert(name: "Peter Parker", role: "Dokter Kandungan",education: "Dokter", educationDesc: "EducationDescription", longExp: 5, expDesc: "Experience Description", price: 20000, starCount: 4.5, imageBase64: (UIImage(named: "UniversalPlaceHolder")?.toBase64())!, isAvailable: false), Expert(name: "Strange Parker", role: "Dokter Kandungan",education: "Dokter", educationDesc: "EducationDescription", longExp: 5, expDesc: "Experience Description", price: 20000, starCount: 4.5, imageBase64: (UIImage(named: "UniversalPlaceHolder")?.toBase64())!, isAvailable: false), Expert(name: "Peter Parker", role: "Dokter Kandungan",education: "Dokter", educationDesc: "EducationDescription", longExp: 5, expDesc: "Experience Description", price: 20000, starCount: 4.5, imageBase64: (UIImage(named: "UniversalPlaceHolder")?.toBase64())!, isAvailable: false), Expert(name: "Peter Parker", role: "Dokter Kandungan",education: "Dokter", educationDesc: "EducationDescription", longExp: 5, expDesc: "Experience Description", price: 20000, starCount: 4.5, imageBase64: (UIImage(named: "UniversalPlaceHolder")?.toBase64())!, isAvailable: false), Expert(name: "Peter Parker", role: "Dokter Kandungan",education: "Dokter", educationDesc: "EducationDescription", longExp: 5, expDesc: "Experience Description", price: 20000, starCount: 4.5, imageBase64: (UIImage(named: "UniversalPlaceHolder")?.toBase64())!, isAvailable: false), Expert(name: "Peter Parker", role: "Dokter Kandungan",education: "Dokter", educationDesc: "EducationDescription", longExp: 5, expDesc: "Experience Description", price: 20000, starCount: 4.5, imageBase64: (UIImage(named: "UniversalPlaceHolder")?.toBase64())!, isAvailable: false) ]
+//    var x : [Expert] = [Expert(name: "Peter Parker", role: "Dokter Kandungan",education: "Dokter", educationDesc: "EducationDescription", longExp: 5, expDesc: "Experience Description", price: 20000, starCount: 4.5, imageBase64: (UIImage(named: "UniversalPlaceHolder")?.toBase64())!, isAvailable: false), Expert(name: "Peter Parker", role: "Dokter Kandungan",education: "Dokter", educationDesc: "EducationDescription", longExp: 5, expDesc: "Experience Description", price: 20000, starCount: 4.5, imageBase64: (UIImage(named: "UniversalPlaceHolder")?.toBase64())!, isAvailable: false), Expert(name: "Strange Parker", role: "Dokter Kandungan",education: "Dokter", educationDesc: "EducationDescription", longExp: 5, expDesc: "Experience Description", price: 20000, starCount: 4.5, imageBase64: (UIImage(named: "UniversalPlaceHolder")?.toBase64())!, isAvailable: false), Expert(name: "Peter Parker", role: "Dokter Kandungan",education: "Dokter", educationDesc: "EducationDescription", longExp: 5, expDesc: "Experience Description", price: 20000, starCount: 4.5, imageBase64: (UIImage(named: "UniversalPlaceHolder")?.toBase64())!, isAvailable: false), Expert(name: "Peter Parker", role: "Dokter Kandungan",education: "Dokter", educationDesc: "EducationDescription", longExp: 5, expDesc: "Experience Description", price: 20000, starCount: 4.5, imageBase64: (UIImage(named: "UniversalPlaceHolder")?.toBase64())!, isAvailable: false), Expert(name: "Peter Parker", role: "Dokter Kandungan",education: "Dokter", educationDesc: "EducationDescription", longExp: 5, expDesc: "Experience Description", price: 20000, starCount: 4.5, imageBase64: (UIImage(named: "UniversalPlaceHolder")?.toBase64())!, isAvailable: false), Expert(name: "Peter Parker", role: "Dokter Kandungan",education: "Dokter", educationDesc: "EducationDescription", longExp: 5, expDesc: "Experience Description", price: 20000, starCount: 4.5, imageBase64: (UIImage(named: "UniversalPlaceHolder")?.toBase64())!, isAvailable: false) ] untuk debug yaa
     
     var body: some View {
         NavigationView{
@@ -24,7 +24,7 @@ struct ConsultationMainPageView: View {
                     VStack {
                         if (!searchBarValue.isEmpty) {
                             VStack {
-                                SearchPage(selectedExpert:  x.filter{
+                                SearchPage(selectedExpert:  viewModel.experts.savedExpert.filter{
                                     $0.name.contains(searchBarValue)
                                 })
                                 .frame(maxHeight: 320)
@@ -41,73 +41,76 @@ struct ConsultationMainPageView: View {
                             }
                         }.pickerStyle(.segmented)
                             .padding(EdgeInsets(top: 2, leading: 18, bottom: 18, trailing: 18))
-                        VStack {
+                        
+                        if (SegmentedPickerValue == "Ongoing"){
+                            VStack {
+                                HStack(){
+                                    Text("\(Prompt.subTitle.recomendedExpert)")
+                                        .font(.system(size: 22, weight: .bold))
+                                        .padding(.leading, 18)
+                                    Spacer()
+                                }.padding(.bottom, 6)
+                                Spacer()
+                                if viewModel.experts.savedExpert.isEmpty {
+                                    Text("No Expert available")
+                                        .padding()
+                                    
+                                } else {
+                                    ForEach(
+                                        // x  //Kalo mau test n mattin if
+                                        viewModel.experts.savedExpert
+                                        , id: \.self) { exp in
+                                            LongExpertCard(ExpertData: exp, buttonText: Prompt.Button.chat)
+                                                .padding(.bottom, 6)
+                                        }
+                                }
+                                Spacer()
+                            }.frame(
+                                height: (viewModel.top3Experts.isEmpty) ? 108 : 370)
+                            
+                            
                             HStack(){
-                                Text("\(Prompt.subTitle.recomendedExpert)")
+                                Text("\(Prompt.subTitle.discoverOther)")
                                     .font(.system(size: 22, weight: .bold))
                                     .padding(.leading, 18)
                                 Spacer()
+                                Button {
+                                    
+                                } label: {
+                                    VStack {
+                                        Text(Prompt.Button.seeAll)
+                                            .font(.system(size: 12, weight: .medium))
+                                            .foregroundColor(AppColor.red)
+                                            .padding(EdgeInsets(top: 3, leading: 14, bottom: 3, trailing: 14))
+                                    }
+                                    .cornerRadius(5)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 5)
+                                            .stroke(AppColor.red, lineWidth: 1)
+                                    )
+                                    .padding(.trailing, 18)
+                                }
                             }.padding(.bottom, 6)
-                            Spacer()
-                            if viewModel.experts.savedExpert.isEmpty {
+                                .padding(.top, 19)
+                            
+                            if (viewModel.experts.savedExpert.isEmpty) {
                                 Text("No Expert available")
-                                    .padding()
+                                    .padding(.vertical, 20)
                                 
                             } else {
-                                ForEach(
-                                    // x  //Kalo mau test n mattin if
-                                    viewModel.experts.savedExpert
-                                    , id: \.self) { exp in
-                                        LongExpertCard(ExpertData: exp, buttonText: Prompt.Button.chat)
-                                            .padding(.bottom, 6)
+                                ScrollView(.horizontal) {
+                                    
+                                    HStack {
+                                        ForEach(
+                                            viewModel.experts.savedExpert.prefix(3)
+                                            //                            x // Use this for testing
+                                            , id: \.self) { expert in
+                                                ShortExpertCard(ExpertData: expert, buttonText: Prompt.Button.chat)
+                                                    .padding()
+                                            }
                                     }
+                                }.frame(height: 170)
                             }
-                            Spacer()
-                        }.frame(
-                            height: (viewModel.top3Experts.isEmpty) ? 108 : 370)
-                        
-                        
-                        HStack(){
-                            Text("\(Prompt.subTitle.discoverOther)")
-                                .font(.system(size: 22, weight: .bold))
-                                .padding(.leading, 18)
-                            Spacer()
-                            Button {
-                                
-                            } label: {
-                                VStack {
-                                    Text(Prompt.Button.seeAll)
-                                        .font(.system(size: 12, weight: .medium))
-                                        .foregroundColor(AppColor.red)
-                                        .padding(EdgeInsets(top: 3, leading: 14, bottom: 3, trailing: 14))
-                                }
-                                .cornerRadius(5)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 5)
-                                        .stroke(AppColor.red, lineWidth: 1)
-                                )
-                                .padding(.trailing, 18)
-                            }
-                        }.padding(.bottom, 6)
-                            .padding(.top, 19)
-                        
-                        if (viewModel.experts.savedExpert.isEmpty) {
-                            Text("No Expert available")
-                                .padding(.vertical, 20)
-                            
-                        } else {
-                            ScrollView(.horizontal) {
-                                
-                                HStack {
-                                    ForEach(
-                                        viewModel.experts.savedExpert.prefix(3)
-                                        //                            x // Use this for testing
-                                        , id: \.self) { expert in
-                                            ShortExpertCard(ExpertData: expert, buttonText: Prompt.Button.chat)
-                                                .padding()
-                                        }
-                                }
-                            }.frame(height: 170)
                         }
                         
                         
