c191eafa4657789f903285d518a460f58ebf6ae9
It's me <74585420+cmp9797@users.noreply.github.com>
2023-05-02 17:35:25 +0700

    promp - modify text, add structs

--- MiniChallenge-Parenthings-6/Core/Constants/Prompt.swift
+++ MiniChallenge-Parenthings-6/Core/Constants/Prompt.swift
@@ -22,6 +22,23 @@ extension Prompt {
 }
 
 extension Prompt {
+    struct Icon {
+        //system icon
+        static let logOut = "rectangle.portrait.and.arrow.right.fill"
+        static let help = "questionmark.circle.fill"
+        
+        static let topUp = "plus.app.fill"
+        static let paymentMethod = "creditcard.fill"
+        static let reedeemCoupon = "ticket.fill"
+        
+        //custom icon
+        
+        
+        private init() {}
+    }
+}
+
+extension Prompt {
     struct paymentContent {
         static let expertFee = "Expert Fee"
         static let adminFee = "Admin Fee"
@@ -35,34 +52,51 @@ extension Prompt {
         
         static let choosePayment = "Choose your payment method"
         
+        static let balance = "Balance: "
+        static let amountPaid = "AMOUNT PAID"
+        
         private init() {}
     }
 }
 
-extension Prompt {
-    struct Button {
-        static let chat = "Chat"
-        static let viewDetail = "View Detail"
-        static let logOut = "Log Out"
-        static let reChatExpert = "Rechat Expert"
-        static let consultNow = "Consult Now"
-        static let seeAll = "See All"
-        static let topUp = "Top Up"
-        static let startConsulting = "Start Consulting"
-        static let logIn = "Log In"
-        static let signUp = "I'm new, sign me up"
-        private init() {}
-    }
-}
+struct Button {
+    static let chat = "Chat"
+    static let viewDetail = "View Detail"
+    static let seeAll = "See All"
+    static let continueProcess = "Continue"
+      
+    static let consultNow = "Consult Now"
+    static let startConsulting = "Start Consulting"
+    static let reChatExpert = "Rechat Expert"
 
+    static let logIn = "Log In"
+    static let logOut = "Log Out"
+    static let signUp = "I'm New, Sign Me Up"
+      
+    static let paymentMethod = "Payment Method"
+    static let help = "Help"
+    static let redeemCoupon = "Redeem Coupon"
+      
+    static let topUp = "Top Up"
+    static let typeTheAmount = "type the amount"
+    static let confirmAndTopUp = "Confirm & Top Up"
+    
+    private init() {}
+}
 
 extension Prompt {
     struct Title {
         static let consultation = "Consultation"
+        static let consultationDetail = "Consultation Detail"
         static let articles = "Articles"
         static let profiles = "Profiles"
-        static let consultationDetail = "Consultation Detail"
-        static let payment  = "Payment"
+        
+        static let logIn = "Log In"
+        static let signUp = "Sign Up"
+        
+        static let payment = "Payment"
+        static let topUp = "Top Up"
+        static let topUpConfirmation = "Top Up Confirmation"
         
         private init() {}
     }
@@ -70,12 +104,41 @@ extension Prompt {
 
 extension Prompt {
     struct subTitle {
-        static let onGoingConsultation = "Ongoging Consultation"
         static let recomendedExpert = "Recomended Expert"
         static let discoverOther = "Discover Other"
+        static let onGoingConsultation = "Ongoging Consultation"
+
         static let trending = "Trending"
         static let latestArticles = "Latest Article"
         static let suggestedForYou  = "Suggested For You"
+        
+        static let enterPhoneNum = "Enter your phone number"
+        static let otpSentToSms = "OTP sent securely to SMS"
+        
+        static let welcomeToApp = "Welcome to Parenthings"
+        static let thanksForUsing = "Thanks for using Parenthings"
+        
+        static let chooseAnAmount = "Choose an amount"
+        static let topUpAmount = "Top up amount"
+        static let selectPaymentMethod = "Select a payment method"
+        static let paymentSuccess = "Payment Successful"
+        
+        
+        private init() {}
+    }
+}
+
+extension Prompt {
+    struct Caption {
+        static let appTagline = "Parenting Wisdom in Your Fingertips"
+        
+        static let welcomeToAppGreetings = "Lorem ipsum dolor sit amet, this is welcomeToAppGreetings variable, go to caption to change text"
+        static let thanksForUsingGratitude = "Hope you've solved the problem, See you next time!"
+        
+        static let enterPhoneNumGuide = "You can log in or make an account if you're new to Parenthings"
+        static let otpSentToSmsGuide = "Enter the OTP we've just sent to"
+
+        static let paymentSuccessCelebration = "Hooray! You have completed your payment"
         
         private init() {}
     }
