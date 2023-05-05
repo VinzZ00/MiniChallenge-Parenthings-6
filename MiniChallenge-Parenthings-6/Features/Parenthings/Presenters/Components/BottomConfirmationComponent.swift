53acf2f9ae1dcdcc48b30f38152f5cb9fc5efb4e
It's me <74585420+cmp9797@users.noreply.github.com>
2023-05-05 14:04:58 +0700

    Update Celine Terbaru

---
+++ MiniChallenge-Parenthings-6/Features/Parenthings/Presenters/Components/BottomConfirmationComponent.swift
@@ -0,0 +1,77 @@
+//
+//  BottomConfirmationComponent.swift
+//  MiniChallenge-Parenthings-6
+//
+//  Created by Celine Margaretha on 04/05/23.
+//
+
+import SwiftUI
+
+struct BottomConfirmationComponent: View {
+    var body: some View {
+        
+        //Top up method confirmation
+        VStack{
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(Prompt.paymentContent.topUpWith)
+                            .font(.callout)
+                            .bold()
+                        Button {
+                            
+                        } label: {
+                            Text(Prompt.Caption.selectTopUpWithGuide)
+                                .foregroundColor(AppColor.red)
+                        }
+                        .buttonStyle(.borderless)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: Prompt.Icon.circleAndThreeDots)
+                                .foregroundColor(AppColor.grayOpacity60)
+                        }
+                        .buttonStyle(.borderless)
+                        
+                    }
+                }
+                
+                //Button confirm and top up
+                VStack {
+                    Button {
+                        
+                    } label: {
+                        Text(Prompt.Button.confirmAndTopUp)
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 15)
+                    }
+                    .font(.title2)
+                    .bold()
+                    .foregroundColor(.white)
+                    .background(AppColor.paymentBlueTextColor)
+                    .buttonStyle(.borderless)
+                    .cornerRadius(15)
+                }
+                .padding(.top, 10)
+            }
+            .padding(.horizontal, 35)
+            .padding(.top, 20)
+        
+        }
+        .background(AppColor.white)
+        .frame(maxWidth: .infinity)
+        .edgesIgnoringSafeArea(.bottom)
+        .shadow(color: AppColor.grayLightColor, radius: 29, x: 0, y: 5)
+    }
+}
+
+struct BottomConfirmationComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomConfirmationComponent()
+    }
+}
