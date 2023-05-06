4847bc5e24cba5273d482ae16b3a844188bd7cae
It's me <74585420+cmp9797@users.noreply.github.com>
2023-05-06 13:24:45 +0700

    modify log button type to easier custom text

--- MiniChallenge-Parenthings-6/Features/Parenthings/Presenters/Components/LogOutButtonComponent.swift
+++ MiniChallenge-Parenthings-6/Features/Parenthings/Presenters/Components/LogOutButtonComponent.swift
@@ -9,38 +9,38 @@ import SwiftUI
 
 struct LogOutButtonComponent: View {
     @Binding var showAlert : Bool
-
+    
     var body: some View {
         Button {
             showAlert = true
         } label: {
-           ImageLabel(imageName: Prompt.Icon.logOut, imageText: Prompt.Button.logOut, isCustom: false)
-               .frame(maxWidth: .infinity)
-               .padding(.vertical, 10)
-               .overlay(
-                   RoundedRectangle(cornerRadius: 10)
-                       .stroke(AppColor.grayLightColor, lineWidth: 1)
-               )
+            ImageLabel(imageName: Prompt.Icon.logOut, imageText: Prompt.Button.logOut, isCustom: false)
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(AppColor.grayLightColor, lineWidth: 1)
+                )
         }
-       .padding(.top, 50)
-       .foregroundColor(AppColor.paymentBlueTextColor)
-       .alert(isPresented: $showAlert) {
-           Alert(
-            title: Text(Prompt.AlertTitle.logOut),
-              message: Text(Prompt.AlertMessages.logOutMessage),
-            primaryButton: .destructive(Text(Prompt.Button.logOutAlert)) {
-                  //action here
-              },
-              secondaryButton: .cancel()
-          )
-           
-       }
-            
+        .padding(.top, 50)
+        .foregroundColor(AppColor.paymentBlueTextColor)
+        .alert(Prompt.AlertTitle.logOut, isPresented: $showAlert) {
+            Button(Prompt.Button.cancel, role: .cancel) {
+                // no action needed here
+            }
+            Button(Prompt.Button.logOutAlert, role: .destructive) {
+                // action log out
+            }
+        
+        } message: {
+            Text(Prompt.AlertMessages.logOutMessage)
+        }
+        
     }
 }
-
-struct LogOutButtonComponent_Previews: PreviewProvider {
-    static var previews: some View {
-        LogOutButtonComponent(showAlert: .constant(true))
+    
+    struct LogOutButtonComponent_Previews: PreviewProvider {
+        static var previews: some View {
+            LogOutButtonComponent(showAlert: .constant(true))
+        }
     }
-}
