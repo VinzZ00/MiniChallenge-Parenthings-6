4d6fa262d2e2010021169a9b93770c07aa0787d5
Rainer Regan (Exacode) <rainercoc@gmail.com>
2023-05-07 15:32:43 +0700

    Perbaiki padding dan nambah progressview

--- MiniChallenge-Parenthings-6/Features/Parenthings/Presenters/Views/OnGoingMainConsultationView.swift
+++ MiniChallenge-Parenthings-6/Features/Parenthings/Presenters/Views/OnGoingMainConsultationView.swift
@@ -49,32 +49,33 @@ struct OnGoingMainConsultationPage: View {
                     Spacer()
                 }
                 .padding(.horizontal, Prompt.Padding.paddingMedium)
-                Spacer()
-                if expertViewModel.isLoading {
-                    ProgressView()
-                } else {
-                    if expertViewModel.experts.isEmpty {
-                        Text("No Expert available")
-                            .padding()
+                
+                VStack(alignment: .center) {
+                    if expertViewModel.isLoading {
+                        ProgressView()
+                            .padding(.vertical, Prompt.Padding.paddingLarge)
                     } else {
-                        ForEach(
-    //                        x[0...2]  //Kalo mau test n mattin if
-                            top3Experts
-                            , id: \.self) { exp in
-                                LongExpertCard(ExpertData: exp, buttonText: Prompt.Button.chat)
-                            }
+                        if expertViewModel.experts.isEmpty {
+                            Text("No Expert available")
+                                .padding()
+                        } else {
+                            ForEach(
+        //                        x[0...2]  //Kalo mau test n mattin if
+                                top3Experts
+                                , id: \.self) { exp in
+                                    LongExpertCard(ExpertData: exp, buttonText: Prompt.Button.chat)
+                                }
+                        }
                     }
                 }
-
-                Spacer()
+                .padding(.bottom, 20)
+                
             }
-            .frame(height:500)
             
             
             HStack(){
                 Text("\(Prompt.subTitle.discoverOther)")
                     .font(.system(size: 22, weight: .bold))
-                    .padding(.leading, 18)
                 Spacer()
                 Button {
                     
@@ -90,29 +91,35 @@ struct OnGoingMainConsultationPage: View {
                         RoundedRectangle(cornerRadius: 5)
                             .stroke(AppColor.paymentBlueTextColor, lineWidth: 1)
                     )
-                    .padding(.trailing, 18)
                 }
-            }.padding(.bottom, 6)
-                .padding(.top, 19)
+            }.padding(.horizontal, Prompt.Padding.paddingMedium)
             
-            if (expertViewModel.experts.isEmpty) {
-                Text("No Expert available")
-                    .padding(.vertical, 20)
-                
+            if expertViewModel.isLoading {
+                ProgressView()
+                    .padding(.vertical, Prompt.Padding.paddingLarge)
             } else {
-                ScrollView(.horizontal) {
+                if (expertViewModel.experts.isEmpty) {
+                    Text("No Expert available")
+                        .padding(.vertical, 20)
                     
-                    HStack {
-                        ForEach(
-                            expertViewModel.experts.prefix(3)
-                            //                            x // Use this for testing
-                            , id: \.self) { expert in
-                                ShortExpertCard(ExpertData: expert, buttonText: Prompt.Button.chat)
-                                    .padding()
-                            }
+                } else {
+                    ScrollView(.horizontal) {
+                        
+                        HStack {
+                            ForEach(
+                                expertViewModel.experts.prefix(3)
+                                //                            x // Use this for testing
+                                , id: \.self) { expert in
+                                    ShortExpertCard(ExpertData: expert, buttonText: Prompt.Button.chat)
+                                }
+                        }
                     }
-                }.frame(height: 170)
+                    .frame(height: 170)
+                    .padding(.horizontal, Prompt.Padding.paddingMedium)
+                }
             }
+            
+            
         }
     }
 }
