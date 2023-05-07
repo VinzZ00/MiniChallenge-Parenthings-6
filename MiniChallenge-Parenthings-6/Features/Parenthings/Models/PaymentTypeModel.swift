cdf3d58cb201c32d45e11f36ab1b11d39987b01c
It's me <74585420+cmp9797@users.noreply.github.com>
2023-05-08 02:19:32 +0700

    Create PaymentTypeModel.swift

---
+++ MiniChallenge-Parenthings-6/Features/Parenthings/Models/PaymentTypeModel.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentTypeModel.swift
+//  MiniChallenge-Parenthings-6
+//
+//  Created by Celine Margaretha on 07/05/23.
+//
+
+import Foundation
+
+struct PaymentTypeModel {
+    var name : String
+    var logo : String
+    var isLogoCustom : Bool
+    var completeLogo : String
+    var isCompleteLogoCustom : Bool
+    var taxFee : Double
+}
